90ffb9043f29788c1f6985ffc8dab2bf42cabe75
Jung-GunSong <jsong781@gmail.com>
2023-08-14 20:26:00 -0400

    first version of completed movie app

--- movie-app.js
+++ movie-app.js
@@ -1,34 +1,51 @@
-//monstrously build form in qQuery
-const movieForm = $("<form>").addClass("movie-form");
+'Use Strict'
+const $movieForm = $("<form>").addClass("movie-form");
+const $appTitle = $("<h1>").addClass("app-title").text("Movie Rating App");
+const $titleInput = $("<input>").addClass("title-input").attr("placeholder", "Movie Title");
+const $ratingInput = $("<input>").addClass("rating-input").attr("placeholder", "Movie Rating");;
+const $submitButton = $("<button>").addClass("submit-button").attr("type", "submit").text("Submit");
 
-const titleInput = $("<input>").addClass("title-input");
-const ratingInput = $("<input>").addClass("rating-input");
-const submitButton = $("<button>").addClass("submit-button").attr("type", "submit");
-
-movieForm.append(titleInput).append(ratingInput).append(submitButton);
-$("body").append(movieForm);
-$("body").append('<div class="rating-area"></div>');
+$movieForm.append($titleInput).append($ratingInput).append($submitButton);
+$("body")
+  .append($appTitle)
+  .append($movieForm)
+  .append('<div class="rating-area"></div>');
 
 
-// make a submit listener
-// captures values of title and rating
-// makes a DOM element with these values AND
-//      a button with the ".remove" class
+$($movieForm).on("submit", addMovie);
 
-$(movieForm).on("submit", addMovie);
 
+/**
+ *
+ * @param {evt} submission - takes in a submission event and places title and rating into
+ * a new div with an associated remove button
+ */
 function addMovie(evt) {
   evt.preventDefault();
 
   const title = $(".title-input").val();
   const rating = $(".rating-input").val();
-  const removeButton = $("<button>").addClass("remove-button");
+  const $removeButton = $("<button>").addClass("remove-button").text("Remove");
 
-  const newFilmDiv = $("<div>")
+  const $newFilmDiv = $("<div>")
     .addClass("movie-entry")
     .text(`Title: ${title}, Rating: ${rating}`)
-    .append(removeButton);
+    .append($removeButton);
 
-  $(".rating-area").append(newFilmDiv);
+  $(".rating-area").append($newFilmDiv);
+  $("form").get(0).reset();
 
+}
+
+// on click, function @ rating-area
+  //function will remove the
+  // evt.target.parent
+$(".rating-area").on("click", ".remove-button", removeEntry);
+
+/**
+ *
+ * @param {evt} click- removes associated movie and rating entry
+ */
+function removeEntry (evt){
+  $(evt.target).parent().remove();
 }
